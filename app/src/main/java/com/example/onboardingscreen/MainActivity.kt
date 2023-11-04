ab254190d2b153973506d4d282a9bea77073d79c
med25ch <1804233@bdeb.qc.ca>
2023-11-04 15:30:27 -0400

    Commit : Initial Commit

---
+++ app/src/main/java/com/example/onboardingscreen/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.onboardingscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.onboardingscreen.ui.theme.OnboardingScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OnboardingScreenTheme {
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyApp(
+    modifier: Modifier = Modifier,
+    names : List<String> = listOf("World","Compose","Hello Mohamed")
+){
+    Surface (
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background)
+    {
+        Column (modifier = modifier.padding(vertical = 4.dp)) {
+            for (name in names){
+                Greeting(name = name)
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    val isExpanded = remember { mutableStateOf(false) }
+    val extraPadding = if (isExpanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .fillMaxWidth()) {
+
+        Row(modifier = Modifier.padding(24.dp)){
+            Column (modifier = Modifier.weight(1f).padding(bottom = extraPadding)){
+                Text(
+                    text = "Hello,",
+                )
+
+                Text(
+                    text = "$name!",
+                )
+            }
+            ElevatedButton(onClick = { isExpanded.value = !isExpanded.value })
+            {
+                Text(if (isExpanded.value) "Show less" else "Show more")
+            }
+        }
+
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OnboardingScreenTheme {
+        Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true,widthDp = 320)
+@Composable
+fun MyAppPreview() {
+    OnboardingScreenTheme {
+        MyApp()
+    }
+}
+
